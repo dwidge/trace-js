7f99cd22641ad632fb8189e811bbb7b22549ea14
dwidge <dwidge+gh@gmail.com>
2025-01-08 19:02:35 +0200

    Fix context not always printing

--- src/trace.ts
+++ src/trace.ts
@@ -1,35 +1,43 @@
 export const trace = <T extends (...args: any[]) => any>(
   message: string,
   fn: T,
-  getContext: (...args: Parameters<T>) => any = (...args: Parameters<T>) => args
+  getContext: (...args: Parameters<T>) => any = (...args: Parameters<T>) =>
+    args,
 ): T =>
   ((...args: Parameters<T>): ReturnType<T> => {
     try {
       return fn(...args);
     } catch (error) {
-      throw new Error(`${message}: ${error instanceof Error ? error.message : String(error)}`, {
-        cause: {
-          error,
-          context: getContext(...args),
+      throw new Error(
+        `${message}: ${error instanceof Error ? error.message : String(error)}`,
+        {
+          cause: {
+            error,
+            context: JSON.stringify(getContext(...args)),
+          },
         },
-      });
+      );
     }
   }) as T;
 
-export const traceAsync = async <T extends (...args: any[]) => Promise<any>>(
+export const traceAsync = <T extends (...args: any[]) => Promise<any>>(
   message: string,
   fn: T,
-  getContext: (...args: Parameters<T>) => any = (...args: Parameters<T>) => args
-): Promise<T> =>
+  getContext: (...args: Parameters<T>) => any = (...args: Parameters<T>) =>
+    args,
+): T =>
   (async (...args: Parameters<T>): Promise<ReturnType<T>> => {
     try {
       return await fn(...args);
     } catch (error) {
-      throw new Error(`${message}: ${error instanceof Error ? error.message : String(error)}`, {
-        cause: {
-          error,
-          context: getContext(...args),
+      throw new Error(
+        `${message}: ${error instanceof Error ? error.message : String(error)}`,
+        {
+          cause: {
+            error,
+            context: JSON.stringify(getContext(...args)),
+          },
         },
-      });
+      );
     }
   }) as T;
