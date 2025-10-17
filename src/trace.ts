e4ece920fc97c743ac8653fc0c3cd301a35852de
dwidge <dwidge+gh@gmail.com>
2025-10-17 17:30:11 +0200

    feat: Use makeErrorMessage in trace functions

--- src/trace.ts
+++ src/trace.ts
@@ -1,15 +1,17 @@
+import { makeErrorMessage } from "./makeErrorMessage.js";
+
 export const trace = <T extends (...args: any[]) => any>(
-  message: string,
+  name: string,
   fn: T,
   getContext: (...args: Parameters<T>) => any = (...args: Parameters<T>) =>
-    args,
+    args.length <= 1 ? args[0] : args,
 ): T =>
   ((...args: Parameters<T>): ReturnType<T> => {
     try {
       return fn(...args);
     } catch (error) {
       throw new Error(
-        `${message}: ${error instanceof Error ? error.message : String(error)}`,
+        makeErrorMessage(name, String(error), getContext(...args)),
         {
           cause: {
             error,
@@ -21,17 +23,17 @@ export const trace = <T extends (...args: any[]) => any>(
   }) as T;
 
 export const traceAsync = <T extends (...args: any[]) => Promise<any>>(
-  message: string,
+  name: string,
   fn: T,
   getContext: (...args: Parameters<T>) => any = (...args: Parameters<T>) =>
-    args,
+    args.length <= 1 ? args[0] : args,
 ): T =>
   (async (...args: Parameters<T>): Promise<ReturnType<T>> => {
     try {
       return await fn(...args);
     } catch (error) {
       throw new Error(
-        `${message}: ${error instanceof Error ? error.message : String(error)}`,
+        makeErrorMessage(name, String(error), getContext(...args)),
         {
           cause: {
             error,
